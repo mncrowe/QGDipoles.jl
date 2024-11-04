26b5c3b9238a3058fc06f0d1a48c5f941be5b8d8
Matthew Crowe <mncrowe93@gmail.com>
2024-11-04 18:53:23 +0000

    added missing comma to make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ makedocs(
   authors = "Matthew N. Crowe",
   modules = [QGDipoles],
  sitename = "QGDipoles.jl",
-   format = Documenter.HTML(assets = ["assets/favicon.ico"])
+   format = Documenter.HTML(assets = ["assets/favicon.ico"]),
     pages = Any[
                 "Home" => "index.md",
 		"Examples" => "Examples.md",
