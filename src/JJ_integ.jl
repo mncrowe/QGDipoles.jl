a41059c7b2f58107d9bd0bee5289a4e8ec7069fe
Matthew Crowe <mncrowe93@gmail.com>
2024-11-04 17:01:18 +0000

    attempt using LaTeX in function descriptions

--- src/JJ_integ.jl
+++ src/JJ_integ.jl
@@ -85,7 +85,7 @@ end
 """
 Function: JJ_int(F, j, k, tol=1e-6)
 
-Evaluates the integral I = ``\int_0^\infty F(\xi) J_{2j+2}(\xi) J_{2k+2}(\xi) d\xi``
+Evaluates the integral I = $\int_0^\infty F(\xi) J_{2j+2}(\xi) J_{2k+2}(\xi) d\xi$
 
 Arguments:
  - F: function to integrate, typically A_func or B_func, Function
